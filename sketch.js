7ca340465a68b117d57c25c02f96c16fb7d70975
unknown <arycmn@gmail.com>
2020-06-23 19:25:28 -0300

    adicionei a animação da personagem e movimento do background

--- sketch.js
+++ sketch.js
@@ -1,7 +1,27 @@
+let imagemCenario;
+let imagemPersonagem;
+let Cenario;
+let somDoJogo;
+let personagem;
+
+  function preload() {
+    imagemCenario = loadImage('imagens/cenario/floresta.png');
+    imagemPersonagem = loadImage('imagens/personagem/correndo.png');
+    frameRate(40);
+    somDoJogo = loadSound('sons/trilha_jogo.mp3');
+}
+
 function setup() {
-    createCanvas(400, 400);
-  }
+   createCanvas(windowWidth , windowHeight);
+   cenario = new cenario(imagemCenario,3);
+   personagem = new Personagem(imagemPersonagem);
+   somDoJogo.loop();
+}
+
+function draw() {
+  cenario.exibe();
+  cenario.move();
   
-  function draw() {
-    background(220);
-  }
+  personagem.exibe();
+  
+}
